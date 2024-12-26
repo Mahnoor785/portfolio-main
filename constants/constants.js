4494db7a3981bb359d9c989bcd0a4818e47e91b7
Mahnoor785 <160884071+Mahnoor785@users.noreply.github.com>
2024-12-27 02:42:28 +0500

    Add files via upload

---
+++ constants/constants.js
@@ -0,0 +1,67 @@
+export const NAME = 'Mahnoor kazim';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'Pakistan',
+  City: 'Karachi',
+  Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'mahnoorkazim@gmail.com',
+  PHONE: '+923159591822',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/mahnoor785',
+  LINKEDIN: 'https://www.linkedin.com/in/mahnoorkazim/',
+  TWITTER: 'https://twitter.com/mahnoorkazim',
+  FACEBOOK: 'https://www.facebook.com/mahnoorkazim',
+};
